fa7a399e2e13e5e7797933bc7b23124dd8255054
Eltun Jalilli <eltunjalilli@gmail.com>
2026-04-13 17:22:20 +0400

    9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""Sum total"""
+''' sum squares'''
+from functools import reduce
 
 
 def summation_i_squared(n):
-    """sum total"""
-    if not isinstance(n, int):
-        return None
+    '''sum squares '''
 
-    if n <= 0:
-        return 0
-    return n*(n+1)*(2*n+1)/6
+    if (type(n) is not int or n < 1):
+        return None
+    # return (summation_i_squared(n-1) + n**2)
+    return round((n * (n + 1) * ((2 * n) + 1)) / 6)
