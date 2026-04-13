f5f8aba9041ff70bf575555237e6773fc372838b
Eltun Jalilli <eltunjalilli@gmail.com>
2026-04-13 16:59:12 +0400

    17

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python3
-"""Module contains the function poly_integral
-that calculates the integral of a polynomial"""
+"""Module that calculates the integral of a polynomial"""
 
 
-def poly_integral(poly):
-    """Calculates the integral of a polynomial"""
-    new_list = [0]
+def poly_integral(poly, C=0):
+    """Returns the integral of a polynomial"""
+
+    # Validation
     if not isinstance(poly, list) or len(poly) == 0:
         return None
-    for i in range(len(poly)):
-        new_list.append(poly[i]/(i+1))
 
-    if len(new_list) == 0:
-        return [0]
-    return new_list
+    if not isinstance(C, int):
+        return None
+
+    for item in poly:
+        if not isinstance(item, (int, float)):
+            return None
+
+    # Integral hesabla
+    result = [C]
+
+    for i in range(len(poly)):
+        val = poly[i] / (i + 1)
+
+        # Əgər tam ədəddirsə → int et
+        if val == int(val):
+            val = int(val)
+
+        result.append(val)
+
+    # Listi "minimal" et (sondakı 0-ları sil)
+    while len(result) > 1 and result[-1] == 0:
+        result.pop()
+
+    return result
