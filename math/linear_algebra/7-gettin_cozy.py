1e711f05fa826fe133e87ff5c08ba2e0e21cf720
Eltun Jalilli <eltunjalilli@gmail.com>
2026-04-21 15:26:03 +0400

    7

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -7,7 +7,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
     if axis == 0:
         if len(mat1[0]) != len(mat2[0]):
             return None
-        return [row[:] for row in mat1] + [row[:] for row in mat2]
+        return [[row[:] for row in mat1] + [row[:] for row in mat2]]
     if axis == 1:
         if len(mat1) != len(mat2):
             return None
