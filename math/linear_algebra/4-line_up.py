beb4192fb00bed3dbb6c565ac8541049696f36d7
Eltun Jalilli <eltunjalilli@gmail.com>
2026-04-21 14:06:27 +0400

    4

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Adding array"""
+
+
+def add_arrays(arr1, arr2):
+    """add"""
+    if len(arr1) != len(arr2):
+        return None
+    list = []
+    for i in range(len(arr1)):
+        list.append(arr1[i] + arr2[i])
+    return list
