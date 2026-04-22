286370970c3f57be1fc72ba75acf82146fe2102f
Eltun Jalilli <eltunjalilli@gmail.com>
2026-04-22 11:16:49 +0400

    13

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Cat's Got Your Tongue"""
+
+
+def np_cat(mat1, mat2, axis=0):
+    """ Cat's Got Your Tongue"""
+    salam = []
+    salam.append([mat1]+[mat2])
+    return salam
