488cb940e7b78a3eab265ef2e6beaddb01dea55a
Eltun Jalilli <eltunjalilli@gmail.com>
2026-04-22 19:36:17 +0400

    102

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -1,20 +1,43 @@
 #!/usr/bin/env python3
-"""Module that concatenates two matrices along a specific axis."""
+"""
+    Concatenates two matrices along a given axis
+    """
 
 
 def cat_matrices(mat1, mat2, axis=0):
-    """Concatenate two matrices along a specific axis."""
-    if axis == 0:
-        if isinstance(mat1[0], list):
-            if len(mat1[0]) != len(mat2[0]):
-                return None
-        return mat1 + mat2
-    if isinstance(mat1[0], list):
-        if len(mat1) != len(mat2):
+    """
+    Concatenates two matrices along a given axis
+    """
+
+    # Helper: get shape of matrix
+    def get_shape(mat):
+        shape = []
+        while isinstance(mat, list):
+            shape.append(len(mat))
+            mat = mat[0]
+        return shape
+
+    # Helper: recursive concatenation
+    def concat(m1, m2, ax):
+        if ax == 0:
+            return m1 + m2
+        return [concat(sub1, sub2, ax - 1) for sub1, sub2 in zip(m1, m2)]
+
+    # Get shapes
+    shape1 = get_shape(mat1)
+    shape2 = get_shape(mat2)
+
+    # Must have same number of dimensions
+    if len(shape1) != len(shape2):
+        return None
+
+    # Check compatibility
+    for i in range(len(shape1)):
+        if i != axis and shape1[i] != shape2[i]:
             return None
-        result = [cat_matrices(mat1[i], mat2[i], axis - 1)
-                  for i in range(len(mat1))]
-        if None in result:
-            return None
-        return result
-    return None
+
+    # Perform concatenation
+    try:
+        return concat(mat1, mat2, axis)
+    except Exception:
+        return None
