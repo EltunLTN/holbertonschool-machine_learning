694398a7a0442f7a106df08ef964436f62799507
Eltun Jalilli <eltunjalilli@gmail.com>
2026-04-22 09:26:36 +0400

    14

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import numpy as np
-"""Saddle up."""
+"""Saddle up.matmul matrixes"""
 
 
 def np_matmul(mat1, mat2):
-    """Saddle up."""
+    """Saddle up.matmul matrixes"""
     return np.matmul(mat1, mat2)
