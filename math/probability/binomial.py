81902a5854dc86f44b4ce9ac955bd9fbbb650204
Eltun <eltunjalilli@gmail.com>
2026-05-05 05:41:53 +0000

    8

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -1,5 +1,53 @@
+#!/usr/bin/env python3
+"""Binomial distribution module"""
 
-def legend(n):
-    ref = (n-1600000)//200000+1
-    return ref
-print(legend(27449580))
+
+class Binomial:
+    """Class that represents a binomial distribution"""
+
+    def __init__(self, data=None, n=1, p=0.5):
+        """
+        Initialize Binomial distribution
+
+        Parameters:
+        data (list): data to estimate n and p
+        n (int): number of trials
+        p (float): probability of success
+        """
+
+        if data is None:
+            # Validate n and p
+            if n <= 0:
+                raise ValueError("n must be a positive value")
+
+            if p <= 0 or p >= 1:
+                raise ValueError("p must be greater than 0 and less than 1")
+
+            self.n = int(n)
+            self.p = float(p)
+
+        else:
+            # Validate data
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+
+            # Compute mean
+            mean = sum(data) / len(data)
+
+            # Compute variance
+            variance = sum((x - mean) ** 2 for x in data) / len(data)
+
+            # Estimate p first
+            p = 1 - (variance / mean)
+
+            # Estimate n
+            n = mean / p
+
+            # Round n (important!)
+            self.n = int(round(n))
+
+            # Recalculate p using rounded n
+            self.p = float(mean / self.n)
--- math/probability/normal.py
+++ math/probability/normal.py
@@ -1,39 +1,23 @@
-#!/usr/bin/env python3
-"""Normal distribution module"""
-
-
-class Normal:
-    """Class that represents a normal distribution"""
-
-    def __init__(self, data=None, mean=0., stddev=1.):
+    def z_score(self, x):
         """
-        Initialize Normal distribution
+        Calculates the z-score of a given x-value
 
         Parameters:
-        data (list): data to estimate mean and stddev
-        mean (float): mean of the distribution
-        stddev (float): standard deviation
+        x (float): x-value
+
+        Returns:
+        float: z-score
         """
+        return (x - self.mean) / self.stddev
 
-        if data is None:
-            # Validate stddev
-            if stddev <= 0:
-                raise ValueError("stddev must be a positive value")
+    def x_value(self, z):
+        """
+        Calculates the x-value of a given z-score
 
-            self.mean = float(mean)
-            self.stddev = float(stddev)
+        Parameters:
+        z (float): z-score
 
-        else:
-            # Validate data
-            if not isinstance(data, list):
-                raise TypeError("data must be a list")
-
-            if len(data) < 2:
-                raise ValueError("data must contain multiple values")
-
-            # Compute mean
-            self.mean = float(sum(data) / len(data))
-
-            # Compute standard deviation
-            variance = sum((x - self.mean) ** 2 for x in data) / len(data)
-            self.stddev = float(variance ** 0.5)
+        Returns:
+        float: x-value
+        """
+        return self.mean + z * self.stddev
