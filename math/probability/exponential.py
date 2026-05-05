12453f8a12a125c2281f366d5700214f17fb9b75
Eltun <eltunjalilli@gmail.com>
2026-05-05 05:38:44 +0000

    4

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -6,28 +6,34 @@ class Exponential:
     """Class that represents an exponential distribution"""
 
     def __init__(self, data=None, lambtha=1.):
-        """
-        Initialize Exponential distribution
-
-        Parameters:
-        data (list): data to estimate lambtha
-        lambtha (float): expected number of occurrences
-        """
-
+        """Initialize Exponential distribution"""
         if data is None:
-            # Validate lambtha
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
-
         else:
-            # Validate data
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
 
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
 
-            # For exponential: lambtha = 1 / mean
             mean = sum(data) / len(data)
             self.lambtha = float(1 / mean)
+
+    def pdf(self, x):
+        """
+        Calculates the PDF for a given time period
+
+        Parameters:
+        x (float): time period
+
+        Returns:
+        float: PDF value for x
+        """
+        # Out of range
+        if x < 0:
+            return 0
+
+        e = 2.7182818285
+        return self.lambtha * (e ** (-self.lambtha * x))
