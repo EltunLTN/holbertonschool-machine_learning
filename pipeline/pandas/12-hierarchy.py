3f1bea788a48e0a76f174f6d50ff398ee433a656
Eltun Jalilli <118074011+EltunLTN@users.noreply.github.com>
2026-05-20 19:48:33 +0400

    Implement hierarchical DataFrame concatenation
    Add a function to concatenate two DataFrames hierarchically.

---
+++ pipeline/pandas/12-hierarchy.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Module for creating a hierarchical concatenation of two DataFrames."""
+import pandas as pd
+index = __import__('10-index').index
+
+
+def hierarchy(df1, df2):
+    """Concatenate bitstamp and coinbase DataFrames with Timestamp as top level.
+
+    Args:
+        df1 (pd.DataFrame): The coinbase DataFrame.
+        df2 (pd.DataFrame): The bitstamp DataFrame.
+
+    Returns:
+        pd.DataFrame: The concatenated DataFrame with Timestamp as top index.
+    """
+    df1 = index(df1)
+    df2 = index(df2)
+    df1 = df1[(df1.index >= 1417411980) & (df1.index <= 1417417980)]
+    df2 = df2[(df2.index >= 1417411980) & (df2.index <= 1417417980)]
+    df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
+    df = df.swaplevel(0, 1).sort_index()
+    return df
