fc195851fbbd4e78867560846a043c38eccb9fed
Eltun Jalilli <118074011+EltunLTN@users.noreply.github.com>
2026-05-20 08:33:10 +0400

    Refactor column renaming to a function
    Refactor the code to define a function for renaming and converting DataFrame columns.

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
-"""Rename the column Timestamp to Datetime
-Convert the timestamp values to datatime values
-Display only the Datetime and Close columns"""
+"""Module for renaming and converting DataFrame columns."""
 import pandas as pd
 
 
-from_file = __import__('2-from_file').from_file
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-df = df.rename(columns={"Timestamp": "Datetime"})
-df["Datetime"] = pd.to_datetime(df["Datetime"], unit='s')
-df = df.loc[:, ["Datetime", "Close"]]
-print(df.tail())
+def rename(df):
+    """Rename Timestamp column to Datetime and convert values to datetime.
+
+    Args:
+        df: pd.DataFrame containing a Timestamp column.
+
+    Returns:
+        Modified pd.DataFrame with only Datetime and Close columns.
+    """
+    df = df.rename(columns={'Timestamp': 'Datetime'})
+    df['Datetime'] = pd.to_datetime(df['Datetime'], unit='s')
+    return df[['Datetime', 'Close']]
