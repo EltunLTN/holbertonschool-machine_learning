f5e9254e0ac8bd09f0f3127ac1963f7d27a52992
Eltun Jalilli <118074011+EltunLTN@users.noreply.github.com>
2026-05-20 19:21:57 +0400

    Update 6-flip_switch.py

--- pipeline/pandas/6-flip_switch.py
+++ pipeline/pandas/6-flip_switch.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
-"""alter the pd.DataFrame such that the rows and
-columns are transposed and the data is sorted in
-reverse chronological order"""
+"""
+Module that flips and switches a DataFrame.
+"""
+
 import pandas as pd
 
 
-from_file = __import__('2-from_file').from_file
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-df = df.sort_values(by="Timestamp", ascending=False).T
-print(df.tail(8))
+def flip_switch(df):
+    """
+    Sorts the DataFrame in reverse chronological order
+    and transposes it.
+
+    Args:
+        df (pd.DataFrame): Input DataFrame.
+
+    Returns:
+        pd.DataFrame: Transformed DataFrame.
+    """
+    return df.sort_index(ascending=False).transpose()
