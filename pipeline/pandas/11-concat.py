f0ccda91d258c5583807586534ce68ce03e90cb1
Eltun Jalilli <118074011+EltunLTN@users.noreply.github.com>
2026-05-20 19:45:29 +0400

    Add function to concatenate bitstamp and coinbase DataFrames
    Introduced a function to concatenate two DataFrames, bitstamp and coinbase, with appropriate indexing.

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module for concatenating two DataFrames."""
+index = __import__('10-index').index
+import pandas as pd
+
+
+def concat(df1, df2):
+    """Concatenate bitstamp and coinbase DataFrames.
+
+    Args:
+        df1 (pd.DataFrame): The coinbase DataFrame.
+        df2 (pd.DataFrame): The bitstamp DataFrame.
+
+    Returns:
+        pd.DataFrame: The concatenated DataFrame with keys.
+    """
+    df1 = index(df1)
+    df2 = index(df2)
+    df2 = df2[df2.index <= 1417411920]
+    return pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
