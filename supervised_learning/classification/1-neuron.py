a54a46d3d81f10b268f0834088f6f349bb6ebf5b
Eltun <eltunjalilli@gmail.com>
2026-07-06 15:21:02 +0400

    s

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -1,17 +1,44 @@
 #!/usr/bin/env python3
-"privatice neuron"
+"""Private neuron"""
+
 import numpy as np
 
 
 class Neuron:
-    """private neuron"""
+    """Class that defines a single neuron performing binary classification"""
 
     def __init__(self, nx):
-        """private neuron"""
+        """
+        Class constructor
+
+        Args:
+            nx (int): number of input features to the neuron
+
+        Raises:
+            TypeError: if nx is not an integer
+            ValueError: if nx is less than 1
+        """
         if not isinstance(nx, int):
-            raise TypeError("nx must be a integer")
+            raise TypeError("nx must be an integer")
         if nx < 1:
-            raise TypeError("nx must be positive")
-        self.__W = np.random.randn()
+            raise ValueError("nx must be positive")
+
+        # Initialize weights with random normal distribution (shape 1 x nx)
+        self.__W = np.random.randn(1, nx)
         self.__b = 0
-        self.__a = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for weights vector"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for bias"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for activated output"""
+        return self.__A
