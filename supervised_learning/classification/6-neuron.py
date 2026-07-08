b2dae45287e02c97843e1b6b9ed0632118ef0baf
Eltun <eltunjalilli@gmail.com>
2026-07-08 10:55:29 +0400

    5,6

--- supervised_learning/classification/6-neuron.py
+++ supervised_learning/classification/6-neuron.py
@@ -90,3 +90,28 @@ class Neuron:
         db = (1 / m) * np.sum(dz)
         self.__W = self.__W - alpha * dW
         self.__b = self.__b - alpha * db
+
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        """
+        Trains the neuron
+
+        X is a numpy.ndarray with shape (nx, m) that contains the input data
+        Y is a numpy.ndarray with shape (1, m) that contains the correct
+        labels for the input data
+        iterations is the number of iterations to train over
+        alpha is the learning rate
+        """
+        if type(iterations) is not int:
+            raise TypeError("iterations must be an integer")
+        if iterations < 0:
+            raise ValueError("iterations must be a positive integer")
+        if type(alpha) is not float:
+            raise TypeError("alpha must be a float")
+        if alpha < 0:
+            raise ValueError("alpha must be positive")
+
+        for i in range(iterations):
+            A = self.forward_prop(X)
+            self.gradient_descent(X, Y, A, alpha)
+
+        return self.evaluate(X, Y)
