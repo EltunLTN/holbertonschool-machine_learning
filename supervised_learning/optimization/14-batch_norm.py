e8055abd452890b8fe1062593564efcb79dd80d7
Eltun <eltunjalilli@gmail.com>
2026-07-21 20:31:45 +0400

    s

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -7,25 +7,25 @@ def create_batch_norm_layer(prev, n, activation):
     """
     Creates a batch normalization layer for a neural network in tensorflow
 
-    Args:
-        prev: activated output of the previous layer
-        n: number of nodes in the layer to be created
-        activation: activation function to be used on the output of the
-            layer
+    prev is the activated output of the previous layer
+    n is the number of nodes in the layer to be created
+    activation is the activation function that should be used
+        on the output of the layer
 
-    Returns:
-        a tensor of the activated output for the layer
+    Returns: a tensor of the activated output for the layer
     """
     init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
     dense = tf.keras.layers.Dense(units=n, kernel_initializer=init)
     Z = dense(prev)
 
-    gamma = tf.Variable(tf.ones([n]), trainable=True, name='gamma')
-    beta = tf.Variable(tf.zeros([n]), trainable=True, name='beta')
+    gamma = tf.Variable(
+        initial_value=tf.ones((1, n)), trainable=True, name='gamma')
+    beta = tf.Variable(
+        initial_value=tf.zeros((1, n)), trainable=True, name='beta')
 
-    epsilon = 1e-7
     mean, variance = tf.nn.moments(Z, axes=[0])
-    Z_norm = tf.nn.batch_normalization(Z, mean, variance, beta, gamma,
-                                        epsilon)
+    epsilon = 1e-7
+    Z_norm = tf.nn.batch_normalization(
+        Z, mean, variance, beta, gamma, epsilon)
 
     return activation(Z_norm)
