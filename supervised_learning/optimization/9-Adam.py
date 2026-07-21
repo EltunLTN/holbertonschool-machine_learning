6d97c8eee516c5c409fd76f05d2acd57707bfa9a
Eltun <eltunjalilli@gmail.com>
2026-07-21 22:12:12 +0400

    s

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -4,8 +4,7 @@ Adam optimization algorithm"""
 import numpy as np
 
 
-def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad,
-                           v, s, t):
+def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     """
     Updates a variable in place using the Adam optimization algorithm
 
