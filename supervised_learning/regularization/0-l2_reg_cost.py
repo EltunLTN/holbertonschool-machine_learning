481fab78230f9647e4f70a173fcbf006d2c8cc28
Eltun <eltunjalilli@gmail.com>
2026-08-01 11:13:10 +0400

    1

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
-"""
-Function to calculate the cost of a neural network with L2 regularization.
-"""
-
+"""L2 regularization cost."""
 import numpy as np
 
 
@@ -10,20 +7,19 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     """
     Calculates the cost of a neural network with L2 regularization.
 
-    Parameters:
-    - cost: numpy.ndarray or float, the cost of the network without L2 regularization
-    - lambtha: float, the regularization parameter
-    - weights: dict, dictionary of the weights and biases of the neural network
-    - L: int, the number of layers in the neural network
-    - m: int, the number of data points used
+    cost: cost of the network without L2 regularization
+    lambtha: regularization parameter
+    weights: dict of the weights and biases (numpy.ndarrays) of the
+             network
+    L: number of layers in the network
+    m: number of data points used
 
-    Returns:
-    - float, the cost of the network accounting for L2 regularization
+    Returns: the cost of the network accounting for L2 regularization.
     """
-    sum_weights_squared = 0
-    for l in range(1, L + 1):
-        W = weights.get('W' + str(l))
-        if W is not None:
-            sum_weights_squared += np.sum(np.square(W))
-    l2_term = (lambtha / (2 * m)) * sum_weights_squared
-    return float(cost + l2_term)
+    weights_sum = 0
+    for layer in range(1, L + 1):
+        W = weights['W' + str(layer)]
+        weights_sum += np.linalg.norm(W) ** 2
+
+    l2_cost = cost + (lambtha / (2 * m)) * weights_sum
+    return l2_cost
