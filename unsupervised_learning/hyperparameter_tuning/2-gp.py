df523bccfd1e6c56f997464eca793b40efab3edc
Eltun <eltunjalilli@gmail.com>
2026-08-19 22:01:45 +0400

    2,3

---
+++ unsupervised_learning/hyperparameter_tuning/2-gp.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""Gaussian Process module."""
+
+import numpy as np
+
+
+class GaussianProcess:
+    """Represents a noiseless 1D Gaussian process."""
+
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """Initialize the Gaussian process."""
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(self.X, self.X)
+
+    def kernel(self, X1, X2):
+        """Calculate the covariance kernel matrix."""
+        sqdist = np.sum(
+            (X1[:, np.newaxis] - X2[np.newaxis, :]) ** 2,
+            axis=2
+        )
+
+        return (self.sigma_f ** 2) * np.exp(
+            -sqdist / (2 * self.l ** 2)
+        )
+
+    def predict(self, X_s):
+        """Predict the mean and variance for points in X_s."""
+        K_s = self.kernel(self.X, X_s)
+        K_ss = self.kernel(X_s, X_s)
+
+        K_inv = np.linalg.inv(self.K)
+
+        mu = K_s.T @ K_inv @ self.Y
+        sigma = K_ss - K_s.T @ K_inv @ K_s
+
+        return mu.reshape(-1), np.diag(sigma)
+
+    def update(self, X_new, Y_new):
+        """Update the Gaussian process with a new sample."""
+        self.X = np.vstack((self.X, X_new))
+        self.Y = np.vstack((self.Y, Y_new))
+        self.K = self.kernel(self.X, self.X)
