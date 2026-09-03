7a9fd28424882aedb5f909af5c54aa713f5856d5
Eltun <eltunjalilli@gmail.com>
2026-09-03 20:49:14 +0400

    s

--- supervised_learning/RNNs/2-gru_cell.py
+++ supervised_learning/RNNs/2-gru_cell.py
@@ -23,23 +23,23 @@ class GRUCell:
     def forward(self, h_prev, x_t):
         """Performs forward propagation for one time step."""
         concat = np.concatenate((h_prev, x_t), axis=1)
-        
+
         # Update gate
         z = 1 / (1 + np.exp(-(np.matmul(concat, self.Wz) + self.bz)))
-        
+
         # Reset gate
         r = 1 / (1 + np.exp(-(np.matmul(concat, self.Wr) + self.br)))
-        
+
         # Candidate hidden state
         concat_r = np.concatenate((r * h_prev, x_t), axis=1)
         h_tilde = np.tanh(np.matmul(concat_r, self.Wh) + self.bh)
-        
+
         # Next hidden state
         h_next = (1 - z) * h_prev + z * h_tilde
-        
+
         # Output
         y_linear = np.matmul(h_next, self.Wy) + self.by
-        
+
         # Softmax activation
         exp_y = np.exp(y_linear - np.max(y_linear, axis=1, keepdims=True))
         y = exp_y / np.sum(exp_y, axis=1, keepdims=True)
