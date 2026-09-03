9571272147ee72bd4b6f94499588d47cedb96bf0
Eltun <eltunjalilli@gmail.com>
2026-09-03 20:43:34 +0400

    2

---
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+RNN Cell Module
+"""
+
+import numpy as np
+
+
+class RNNCell:
+    """Represents a cell of a simple RNN."""
+
+    def __init__(self, i, h, o):
+        """Class constructor."""
+        self.Wh = np.random.randn(i + h, h)
+        self.Wy = np.random.randn(h, o)
+        self.bh = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """Performs forward propagation for one time step."""
+        concat = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(np.matmul(concat, self.Wh) + self.bh)
+
+        y_linear = np.matmul(h_next, self.Wy) + self.by
+        
+        # Softmax activation
+        exp_y = np.exp(y_linear - np.max(y_linear, axis=1, keepdims=True))
+        y = exp_y / np.sum(exp_y, axis=1, keepdims=True)
+
+        return h_next, y
